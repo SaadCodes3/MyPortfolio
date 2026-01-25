996d998ba7bd042e9de29b5fbf157133aa2ca103
SaadCodes3 <saad.elhusseiny1451@compit.aun.edu.eg>
2026-01-25 15:00:41 +0200

    Add scroll to top button

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const toggleBtn = document.getElementById("theme-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+//scroll button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
