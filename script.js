56f5b71a529d970a77798806037ebefa64b9476d
Saad Alhusseiny <saadalhusseiny1@gmail.com>
2026-02-01 15:12:22 +0200

    feat: add close button to mobile navbar menu

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const toggleBtn = document.getElementById("theme-switch");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
 //menu-button
 const menuBtn =document.querySelector('.menu-btn');
 const nav = document.querySelector(".navigation");
@@ -11,23 +12,26 @@ menuBtn.addEventListener("click",()=>{
   nav.classList.toggle("open");
 });
 const navLinks = document.querySelectorAll(".navigation a");
-
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
     nav.classList.remove("open");
   });
 });
-
-
 document.addEventListener("click", (e) => {
   if (!nav.contains(e.target) && !menuBtn.contains(e.target)) {
     nav.classList.remove("open");
   }
 });
 
-//scroll-to-top button
-const scrollBtn = document.getElementById("scrollTopBtn");
+// close button 
+const closeBtn =document.querySelector('.close-btn');
+closeBtn.addEventListener("click",()=>{
+    nav.classList.remove("open");
+})
 
+
+//scroll-to-top button display
+const scrollBtn = document.getElementById("scrollTopBtn");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 300) {
     scrollBtn.style.display = "block";
@@ -35,7 +39,7 @@ window.addEventListener("scroll", () => {
     scrollBtn.style.display = "none";
   }
 });
-
+//scroll-to-top button move
 scrollBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
