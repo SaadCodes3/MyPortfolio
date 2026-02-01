b9e88829884ac6210dca428b0f36a624789fa246
Saad Alhusseiny <saadalhusseiny1@gmail.com>
2026-02-01 20:37:19 +0200

    Improve the footer in mobile and tablet

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 const nav = document.querySelector(".navigation");
 const scrollBtn = document.getElementById("scrollTopBtn");
+const navLinks = document.querySelectorAll(".navigation a");
 
+
+// ===== Dark Mode with LocalStorage =====
+
+
+// عند تحميل الصفحة
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+// عند الضغط على الزرار
 // ===== Dark Mode =====
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+
+
+
+  // حفظ الوضع
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 });
+
 
 // ===== Mobile Menu =====
 menuBtn.addEventListener("click", () => {
@@ -18,7 +39,18 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   nav.classList.remove("open");
 });
-
+//for any place exept nav && menu button
+document.addEventListener("click",(e) => {
+  if(!nav.contains(e.target) && !menuBtn.contains(e.target)){
+    nav.classList.remove("open");
+  }
+})
+//for link click
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("open");
+  });
+});
 // ===== Scroll Button =====
 window.addEventListener("scroll", () => {
   scrollBtn.style.display = window.scrollY > 300 ? "block" : "none";
