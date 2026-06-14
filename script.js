30bcf87ae87fd24885bd3edc3846d75b5595d0af
SaadAlhusseiny <saad.elhusseiny1451@compit.aun.edu.eg>
2026-06-14 23:21:55 +0300

    improve responsive design for skills and projects sections

--- script.js
+++ script.js
@@ -94,3 +94,24 @@ function typeEffect() {
 }
 
 typeEffect();
+
+// ===== Skills Slider =====
+const slider = document.querySelector(".content.slider");
+
+let isDown = false, startX, scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => isDown = false);
+slider.addEventListener("mouseup", () => isDown = false);
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  slider.scrollLeft = scrollLeft - (x - startX);
+});
